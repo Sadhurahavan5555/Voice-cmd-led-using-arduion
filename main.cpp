8eb5fda8e950b1ca9bdd3d2f9c7595b15ee85c62
Sadhurahavan5555 <109868957+Sadhurahavan5555@users.noreply.github.com>
2023-09-29 20:06:58 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char auth[] = "YOUR_BLYNK_AUTH_TOKEN";
+char ssid[] = "YOUR_WIFI_SSID";
+char pass[] = "YOUR_WIFI_PASSWORD";
+
+void setup() {
+  Serial.begin(115200);
+  Blynk.begin(auth, ssid, pass);
+}
+
+void loop() {
+  Blynk.run();
+}
+
+BLYNK_WRITE(V1) {
+  int pinValue = param.asInt();
+  digitalWrite(D3, pinValue); // Control the LED
+}
+
+BLYNK_WRITE(V2) {
+  String voiceCommand = param.asStr();
+  if (voiceCommand == "turn on the light") {
+    Blynk.virtualWrite(V1, 1); // Turn on the LED
+  } else if (voiceCommand == "turn off the light") {
+    Blynk.virtualWrite(V1, 0); // Turn off the LED
+  }
+}
